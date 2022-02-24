80346fcafe3bcca20856ec08c8e7fd7b31bad39e
rzondervan <rzondervan@gmail.com>
2022-02-24 22:58:35 +0100

    eerste commit vanaf mac

--- gitsetup.sh
+++ gitsetup.sh
@@ -2,5 +2,10 @@ git config --global user.name "rzondervan"
 git config --global user.email "rzondervan@gmail.com"
 git config --global push.default matching
 git config --global alias.co checkout
-git init
 
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:rzondervan/privacy_app.git
+git push -u origin main
