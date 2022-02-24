46375cbe027b3cc251b74ced25b998166f348441
rzondervan <rzondervan@gmail.com>
2022-02-24 22:46:55 +0100

    eerste commit vanaf mac

---
+++ android/app/src/main/kotlin/privacyvideo/nl/privacy_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package privacyvideo.nl.privacy_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gitsetup.sh
@@ -0,0 +1,6 @@
+git config --global user.name "rzondervan"
+git config --global user.email "rzondervan@gmail.com"
+git config --global push.default matching
+git config --global alias.co checkout
+git init
+
